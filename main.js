373bcca02779a7ca2411962cb8c7c42a58da488c
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2023-05-06 01:12:59 +0300

    Updated main.js

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ import App from './src/App'
 
 
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
